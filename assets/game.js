95193e2aeeecc851f7a12c3dcad953e75f31f66e
mjd10m <mjd10m@outlook.com>
2022-06-26 12:08:26 -0400

    first battle

--- assets/game.js
+++ assets/game.js
@@ -1,11 +1,28 @@
-var playerName = window.prompt("What's your Robots's name?");
+var playerName = window.prompt("What's your robots name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+console.log(playerName, playerHealth, playerAttack);
 
-// this creates a function named fight
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-function fight() {
-    window.alert("The fight has begun");
-}
+var fight = function() {
+    window.alert("welcome to the Robot Gladiators");
 
-//fight();
+    //subtract the value of 'playerAttack' 'from enemyHealth' and use that result to update 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
+    //log a resulting message to console
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining"
+    )
+    //subtract the value of 'enemyAttack' 'from playerHealth' and use that result to update 'playerHealth' variable
+    playerHealth = playerHealth - enemyAttack;
+    //log a resulting message to console
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining"
+    )
+};
+
+fight()
